97790ba8ce8c4c61caaebfcea1672c67b11cc789
Tom <mail@alpers.it>
2022-09-23 00:15:44 +0200

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true }); // TODO: cors settings for prod
 
   const config = new DocumentBuilder().setTitle('LorDecks API').build();
   const document = SwaggerModule.createDocument(app, config);
