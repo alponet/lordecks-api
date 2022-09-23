f86408c72bbf076ac0221f132a8bece7d1f7b39e
Tom <mail@alpers.it>
2022-09-23 21:35:51 +0200

    use config service for database credentials

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,18 @@ import { TasksModule } from './tasks/tasks.module';
 import { MatchesModule } from './matches/matches.module';
 import { DecksModule } from "./decks/decks.module";
 import { ScheduleModule } from "@nestjs/schedule";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://USER:PASSWORD@URL/?retryWrites=true&w=majority',
-      { dbname: 'DBNAME' },
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ ConfigModule ],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get('DB_URI'),
+      }),
+      inject: [ ConfigService ]
+    }),
     ScheduleModule.forRoot(),
     TasksModule,
     MatchesModule,
