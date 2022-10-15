fbf2bf1a0b47f4092077eedbbbc532c6c469f50c
Tom <mail@alpers.it>
2022-10-15 20:44:27 +0200

    POST endpoint for card errors

--- src/decks/decks.controller.ts
+++ src/decks/decks.controller.ts
@@ -1,14 +1,22 @@
 import { DecksService } from './decks.service';
-import { Controller, Get, Param } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 
 
 @Controller('decks')
 export class DecksController {
+
   constructor(private decksService: DecksService) {}
+
 
   @Get('byCode/:deckcode')
   async getDeckFromCode(@Param('deckcode') deckcode: string): Promise<any[]> {
     return this.decksService.convertToDeck(deckcode);
+  }
+
+
+  @Post('cardError')
+  reportCardError(@Body() body: { cardCode: string }): void {
+    console.error("cardError posted: " + body.cardCode);
   }
 
 }
